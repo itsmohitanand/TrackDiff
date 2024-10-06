1232c8293c24ab286fde2c61f2faf493b531ecaf
melioristic <itsmohitanand@gmail.com>
2024-10-06 18:58:37 -0400

    added basic LSTM prediction

--- td/model/model.py
+++ td/model/model.py
@@ -20,3 +20,30 @@ class MLP(nn.Module):
         x = self.fc6(x)
         x = x.reshape(-1, 6,200)
         return x
+
+
+class TrackGenerator(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.initializer = nn.Sequential(
+            nn.Linear(4, 512),
+            nn.ReLU(),
+            nn.Linear(512, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 7*200),
+        )
+
+        self.sequence_encoder = nn.LSTM(input_size=7, hidden_size=256, num_layers=1, batch_first=True)
+        self.sequence_generator = nn.LSTM(input_size=256, hidden_size=7, num_layers=1, batch_first=True)
+        
+    def forward(self, x):
+        x = self.initializer(x)
+        x = x.reshape(-1, 200, 7)
+        x, _ = self.sequence_encoder(x)
+        x, _ = self.sequence_generator(x)
+
+        sequence = x[:,:,0:6]
+        log_probits = torch.sigmoid(x[:,:,6])
+
+        return sequence, log_probits
--- td/model/model_pl.py
+++ td/model/model_pl.py
@@ -3,6 +3,8 @@ import torch.nn as nn
 import lightning as L
 import torch
 import numpy as np
+from td.model.model import TrackGenerator
+from torch.nn import MSELoss, BCELoss
 
 class MLPL(L.LightningModule):
     def __init__(self):
@@ -18,6 +20,7 @@ class MLPL(L.LightningModule):
         x, y = batch
    
         y_hat = self.model(x)
+
         rmse = self.masked_rmse(y_hat, y)
         dist_loss = self.distance_between_points(y_hat)
 
@@ -64,3 +67,83 @@ class MLPL(L.LightningModule):
         dist = torch.hypot(d[:,0,:], d[:,1,:])
 
         return dist.mean()
+    
+
+class TrackGeneratorL(L.LightningModule):
+    def __init__(self):
+        super(TrackGeneratorL, self).__init__()
+        self.model = TrackGenerator()
+
+    def forward(self, x):
+        sequence, logits = self.model(x)
+        return sequence, logits
+
+    def batch_transform(self, batch):
+        x, y = batch
+        y = y.permute(0,2,1)
+        return x, y
+    def training_step(self, batch , batch_idx):
+
+        x, y = self.batch_transform(batch)
+   
+        sequence, log_probits = self.model(x)
+
+        rmse = self.masked_rmse(sequence, y)
+        bce = self.masked_bce(log_probits, y)
+
+        loss = rmse +  bce
+
+        self.log("train/loss", loss, prog_bar=True)
+        self.log("train/bce", bce, prog_bar=True)
+        self.log("train/rmse", rmse, prog_bar=True)
+        
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        x,y = self.batch_transform(batch)
+
+        sequence, log_probits = self.model(x)
+        rmse = self.masked_rmse(sequence, y)
+        bce = self.masked_bce(log_probits, y)
+        loss = rmse + bce
+
+        self.log("val/loss", loss, prog_bar=True)
+        self.log("val/bce", bce, prog_bar=True)
+        self.log("val/rmse", rmse, prog_bar=True)
+        return loss
+    
+    def configure_optimizers(self):
+        # optimizer and scheduler
+
+        optimizer = torch.optim.Adam(self.parameters(), lr=0.00005)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=10, verbose=True)
+
+        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val/loss"}
+    
+    def masked_rmse(self, y_pred, y_true):
+        # Create a mask where true values are not equal to the mask value
+        mask = ~torch.isnan(y_true)
+        
+        # Apply the mask to both y_true and y_pred
+        y_true_masked = y_true[mask]
+        y_pred_masked = y_pred[mask]
+
+        # Calculate the RMSE
+        mse = torch.mean((y_true_masked - y_pred_masked) ** 2)
+        rmse = torch.sqrt(mse)
+        
+        return rmse
+    
+    
+    def masked_bce(self, y_pred, y_true):
+
+        # Find seequence length based on first non nan values
+        seq_len =  torch.argmin(torch.nan_to_num(y_true[:, :, 0], -1), dim=1)
+        # Create a one hot encoding for sequence length
+        y_true = torch.nn.functional.one_hot(seq_len, num_classes=200).to(torch.float32)
+
+        # Calculate the binary cross entropy
+        bce = BCELoss()(y_pred, y_true)
+        return bce
+
+    
